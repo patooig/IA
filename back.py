013875b9f612e6aab2683247194341ac22936ce9
patooig <patooig@gmail.com>
2022-06-12 16:24:26 -0400

    CAMBIOS

--- back.py
+++ back.py
@@ -2,16 +2,18 @@ from random import randint
 from time import sleep
 
 #initialise empty 9 by 9 grid
+# 
+
 matriz = []
-matriz.append([3, 0, 6, 5, 0, 8, 4, 0, 0])
-matriz.append([5, 2, 0, 0, 0, 0, 0, 0, 0])
-matriz.append([0, 8, 7, 0, 0, 0, 0, 3, 1])
-matriz.append([0, 0, 3, 0, 1, 0, 0, 8, 0])
-matriz.append([9, 0, 0, 8, 6, 3, 0, 0, 5])
-matriz.append([0, 5, 0, 0, 9, 0, 6, 0, 0])
-matriz.append([1, 3, 0, 0, 0, 0, 2, 5, 0])
-matriz.append([0, 0, 0, 0, 0, 0, 0, 7, 4])
-matriz.append([0, 0, 5, 2, 0, 6, 3, 0, 0])
+matriz.append([5, 8, 6, 0, 7, 0, 0, 0, 0])
+matriz.append([0, 0, 0, 9, 0, 1, 6, 0, 0])
+matriz.append([0, 0, 0, 6, 0, 0, 0, 0, 0])
+matriz.append([0, 0, 7, 0, 0, 0, 0, 0, 0])
+matriz.append([9, 0, 2, 0, 1, 0, 3, 0, 5])
+matriz.append([0, 0, 5, 0, 9, 0, 0, 0, 0])
+matriz.append([0, 9, 0, 0, 4, 0, 0, 0, 8])
+matriz.append([0, 0, 3, 5, 0, 0, 0, 6, 0])
+matriz.append([0, 0, 0, 0, 2, 0, 4, 7, 0])
 
 
 def printMatriz(matriz):
@@ -28,15 +30,16 @@ def verificarMatriz(matriz):
         if matriz[fila][columna]==0:
           return False
 
-  #We have a complete grid!  
   return True 
 
-#A backtracking/recursive function to check all possible combinations of numbers until a solution is found
+#BACKTRACCKING
+
+#INICIO
+
 def solveGrid(grid):
-  #Find next empty cell
   for i in range(0,81):
-    fila=i//9
-    columna=i%9
+    fila=i//9      #Divisiòn entera
+    columna=i%9    #Resto de divisiòn
     if grid[fila][columna]==0:
       for val in range (1,10):
         #Check that this value has not already be used on this row
@@ -80,7 +83,7 @@ def solveGrid(grid):
   grid[fila][columna]=0  
   
 
-sleep(1)
+#FIN DE TIEMPO
 
 solved = solveGrid(matriz)
 if solved:
